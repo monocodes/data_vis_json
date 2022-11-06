317e727e01d266a23e873f8e6e605633659d769f
serjCookies <locobu@icloud.com>
2022-11-06 18:20:38 +0400

    adding hover text

--- eq_world_map.py
+++ eq_world_map.py
@@ -10,16 +10,18 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
-# Map the earthqueakes.
+# Map the earthquakes.
 
 # the simplest way to define the data for a chart in Plotly 
 #data = [Scattergeo(lon=lons, lat=lats)]
@@ -29,6 +31,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
