1603f97a2be717005cf8e81546d45230ebaf1990
serjCookies <locobu@icloud.com>
2022-11-17 19:33:50 +0400

    16-6. Refactoring

--- TRY IT YOURSELF/16-2 Mapping Global Data Sets: JSON Format/16-6. Refactoring/eq_world_map.py
+++ TRY IT YOURSELF/16-2 Mapping Global Data Sets: JSON Format/16-6. Refactoring/eq_world_map.py
@@ -21,17 +21,6 @@ for eq_dict in all_eq_dicts:
     lons.append(eq_dict['geometry']['coordinates'][0])
     lats.append(eq_dict['geometry']['coordinates'][1])
     hover_texts.append(eq_dict['properties']['title'])
-"""
-for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
-"""
 
 # Map the earthquakes.
 
