79e48237496a04fd48762d740a69d5833d578219
serjCookies <locobu@icloud.com>
2022-11-28 18:50:51 +0400

    Update notes.sh
    little formatting

--- notes.sh
+++ notes.sh
@@ -1,4 +1,5 @@
 ### Examining JSON DATA ------------------------
+### NOTE ###
 """
 When we talk about locations, we often say the location’s latitude first, followed by the longitude. This convention probably arose because humans discovered latitude long before we developed the concept of longitude. However, many geospatial frameworks list the longitude first and then the latitude, because this corresponds to the (x, y) convention we use in mathematical representations. The geoJSON format follows the (longitude, latitude) convention, and if you use a different framework it’s important to learn what convention that framework follows.
 """
